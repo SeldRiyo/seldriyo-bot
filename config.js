91049e644f3e0f959b300fd0095d10e59f905da9
SeldRiyo <50526840+SeldRiyo@users.noreply.github.com>
2019-09-14 20:47:43 +0430

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'NTQ5MjkxNzE1MTE5Mjg0Mjc0.XXzANg.LHnBhy2H5KxCdy7Wo49N41GjqYs';
+exports.TOKEN = 'NTQ5MjkxNzE1MTE5Mjg0Mjc0.XX0SUQ.GwaWqIv7n10lfWPNqJ2P5xIlmjw';
 
 exports.PREFIX = '$';
 
