b712edd8f09c46072bde9b36109aa3a63aeca415
SeldRiyo <50526840+SeldRiyo@users.noreply.github.com>
2019-09-14 20:04:14 +0430

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NTQ5MjkxNzE1MTE5Mjg0Mjc0.XXzANg.LHnBhy2H5KxCdy7Wo49N41GjqYs';
 
 exports.PREFIX = '$';
 
-exports.GOOGLE_API_KEY = 'AIzaSyAylUv6pC_BanNndbwpO9bnH70ySSXYqNw';
+exports.GOOGLE_API_KEY = 'AIzaSyBdncRcfFgRUTO1gTYKhgSsS06-vr58QDI';
