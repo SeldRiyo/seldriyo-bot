de788c7a942006143f2bf1b5f89af5dcc9b47974
SeldRiyo <50526840+SeldRiyo@users.noreply.github.com>
2019-09-17 10:35:18 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers. type !help`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers. type !help`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers. type !help`);
 });
 
 
@@ -134,10 +134,10 @@ client.on("message", async message => {
   }
   
   if(command === "link") {
-	  message.replay(`https://discord.gg/q6bQpth`)
+	  message.channel.send(`https://discord.gg/q6bQpth`)
   }
   if(command === "help") {
-	  message.replay(`>>> '-IR-' SeldRiyo:registered:
+	  message.channel.send(`>>> '-IR-' SeldRiyo:registered:
 hey guy! there are some commands of me :D 
 
 
