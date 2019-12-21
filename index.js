12ff7114da46f981e4f8d4810c04086e839503d4
SeldRiyo <koushan405@gmail.com>
2019-12-21 20:05:42 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const http = require('http');
+
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(console.log.bind(console, 'tick'), 1000);
+http.createServer((req, res) => res.end('hi'))
+  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`${ signal }...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log('...waited 5s, exiting.');
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
