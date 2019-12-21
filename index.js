cb2eea242f75a29a0d8790fc1da56d4d710ee496
SeldRiyo <koushan405@gmail.com>
2019-12-21 19:33:59 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
-client.on('ready', () => {
-	client.user.setStatus('dnd', 'Made by SeldRiyo'); 
-}
+
+client.on("ready", () => {
+  client.user.setActivity('dnd', 'Made by SeldRiyo');
+});
 
 client.on("message", async message => {
-
   if(message.author.bot) return;
-  
+
   if(message.content.indexOf(prefix) !== 0) return;
-  
+
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
@@ -81,16 +81,16 @@ client.on("message", async message => {
   if (command == 'help') {
     message.react(':ok_hand:');
 let botembed = new Discord.RichEmbed()
-   .setTitle('[BT] SeldRiyo:registered: Bot. ');
-   .setDescription(`prefix < **${prefix}** >`);
-   .setColor('purple');
-   .setURL('http://bit.ly/SeldRiyo');
-   .addField('help','Help you :D');
-   .addField('link',`'-IR-' B3ST Server Link!!`);
-   .addField('kick','Kick somebody from server');
-   .addField('ban','Ban somebody from server');
-   .addField('del','Delete 2 - 100 messeages from server');
-   .addField('ping','Show your latancy and the API latnecy!!');
+   .setTitle('[BT] SeldRiyo:registered: Bot. ')
+   .setDescription('prefix **$**')
+   .setColor('purple')
+   .setURL('http://bit.ly/SeldRiyo')
+   .addField('help','Help you :D')
+   .addField('link',`'-IR-' B3ST Server Link!!`)
+   .addField('kick','Kick somebody from server')
+   .addField('ban','Ban somebody from server')
+   .addField('del','Delete 2 - 100 messeages from server')
+   .addField('ping','Show your latancy and the API latnecy!!')
    .setFooter(`${message.author.username}`, message.author.displayAvatarURL);
    return message.author.send(botembed);
     }
