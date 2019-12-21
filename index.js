6596946abab078cf39f9f2c799611e51dc9228eb
SeldRiyo <koushan405@gmail.com>
2019-12-21 19:35:27 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "$";
 
 client.on("ready", () => {
-  client.user.setActivity('Made by SeldRiyo','dnd);
+  client.user.setActivity('Made by SeldRiyo','dnd');
 });
 
 client.on("message", async message => {
